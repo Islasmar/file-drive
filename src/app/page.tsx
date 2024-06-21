f91eeff42a486fa0d337b9d81ac01161fd8469d0
Mariano Fajardo Islas <mfajardoi.prac@xcaret.com>
2024-06-21 09:40:35 -0500

    Creando página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,40 @@
-import { SignInButton, SignOutButton, SignedIn, useSession } from "@clerk/nextjs";
+'use client'
+import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization } from "@clerk/nextjs";
 import { Button } from "../components/ui/button";
+import { createFile, getFiles } from '../../convex/files';
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Home() {
-
-  const session = useSession();
-
+  const { organization } = useOrganization();
+  const files = useQuery(
+    api.files.getFiles,
+     organization?.id ? { orgId: organization.id } : "skip");
+  const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedIn>
-        <SignOutButton></SignOutButton>
+        <SignOutButton>
+          <Button>Sing Out</Button>
+        </SignOutButton>
       </SignedIn>
-      <Button>Sign Out</Button>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign Int</Button>
+        </SignInButton>
+      </SignedOut>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>
+      })}
+
+      <Button onClick={() => {
+        if (!organization) return;
+        createFile({
+          name: "hello word",
+          orgId: organization.id,
+        });
+      }}>Click Me</Button>
     </main>
-  )}
+  )
+}
