9e20c1d1952a8db26eef0adaf8d97fa5e2c0c92f
Mariano Fajardo Islas <mfajardoi.prac@xcaret.com>
2024-07-08 16:42:28 -0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,37 @@ import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
 export default function Home() {
-  const organization  = useOrganization();
-  const user =useUser();
+  const organization = useOrganization();
+  const user = useUser();
 
   let orgId: string | undefined = undefined;
-  if (organization.isLoaded && user.isLoaded){
+  if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id
-  } 
+  }
 
-  const files = useQuery(api.files.getFiles,orgId ? { orgId } : "skip");
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container mx-auto pt-12">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold">Your Files</h1>
+        <Button onClick={() => {
+          if (!orgId) return;
+          createFile({
+            name: "hello word",
+            orgId,
+          });
+        }}>
+          Upload File
+        </Button>
+      </div>
 
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>
       })}
 
-      <Button onClick={() => {
-        if (!orgId) return;
-        createFile({
-          name: "hello word",
-          orgId,
-        });
-      }}>Click Me</Button>
+
     </main>
   )
 }
